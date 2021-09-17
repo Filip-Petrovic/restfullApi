dc5c4159e010bd890aa4258f9041fda204680b8f
FilipPn97 <FilipPn97@users.noreply.github.com>
2021-09-17 23:37:55 +0200

    Prvi komit

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
 
 require 'vendor\autoload.php';
+include 'bootsrap.php';
+
+use Chatter\Middleware\Logging as ChatterLogging;
+use Chatter\Models\Message;
 
 $app = new \Slim\App();
+$app->add(new ChatterLogging());
+$app->get('/messages', function($request, $response, $args) {
+	try{
 
-$app->get('/hello/{name}', function($request, $response, $args) {
-	return $response->write("hello ". $args['name']);
+	$msg = new Message();
+	$messages = $msg->all();
+
+	}
+	catch(Exception $e){
+		return $response->write($e->getMessage());
+	}
+	$payload = [];
+	foreach($messages as $m) {
+		$payload[$m->id] = [
+			"body" => $m->body,
+			"user_id" => $m->user_id,
+			"created_at" => $m->created_at
+		];
+	}
+	return $response->withStatus(200)->withJson($payload);
 });
 
 $app->run();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,29 +7,73 @@ namespace Composer\Autoload;
 class ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016
 {
     public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Contracts\\Translation\\' => 30,
+            'Symfony\\Component\\Translation\\' => 30,
             'Slim\\' => 5,
         ),
         'P' => 
         array (
+            'Psr\\SimpleCache\\' => 16,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
+        ),
+        'I' => 
+        array (
+            'Illuminate\\Support\\' => 19,
+            'Illuminate\\Database\\' => 20,
+            'Illuminate\\Contracts\\' => 21,
+            'Illuminate\\Container\\' => 21,
         ),
         'F' => 
         array (
             'FastRoute\\' => 10,
         ),
+        'D' => 
+        array (
+            'Doctrine\\Inflector\\' => 19,
+            'Doctrine\\Common\\Inflector\\' => 26,
+        ),
+        'C' => 
+        array (
+            'Chatter\\' => 8,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Contracts\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation-contracts',
+        ),
+        'Symfony\\Component\\Translation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/translation',
+        ),
         'Slim\\' => 
         array (
             0 => __DIR__ . '/..' . '/slim/slim/Slim',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
         ),
         'Psr\\Http\\Message\\' => 
         array (
@@ -39,13 +83,52 @@ class ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
+        'Illuminate\\Support\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/support',
+        ),
+        'Illuminate\\Database\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/database',
+        ),
+        'Illuminate\\Contracts\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/contracts',
+        ),
+        'Illuminate\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/illuminate/container',
+        ),
         'FastRoute\\' => 
         array (
             0 => __DIR__ . '/..' . '/nikic/fast-route/src',
         ),
+        'Doctrine\\Inflector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Inflector',
+        ),
+        'Doctrine\\Common\\Inflector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Common/Inflector',
+        ),
+        'Chatter\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src/Chatter',
+        ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/..' . '/nesbot/carbon/src',
     );
 
     public static $prefixesPsr0 = array (
+        'U' => 
+        array (
+            'UpdateHelper\\' => 
+            array (
+                0 => __DIR__ . '/..' . '/kylekatarnls/update-helper/src',
+            ),
+        ),
         'P' => 
         array (
             'Pimple' => 
@@ -56,7 +139,11 @@ class ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016
     );
 
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -64,6 +151,7 @@ class ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitaa1449e198bb1b6b5a68a548d623a016::$classMap;
 
